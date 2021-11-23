5b16658fc4d2043f0993d348abff73cdfbe14e77
Erik <its.krei@gmail.com>
2021-11-23 15:16:24 -0600

    fixed login screen

--- lib/Screens.dart/login.dart
+++ lib/Screens.dart/login.dart
@@ -14,6 +14,18 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     //Size size = MediaQuery.of(context).size;
+    var column = Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                // in progress: will be to view history
+                context,
+                MaterialPageRoute(builder: (context) => ActionMenu()));
+          },
+        ),
+      ],
+    );
     return Scaffold(
       backgroundColor: Color(0xfff8EE4AF),
       body: Center(
@@ -34,22 +46,81 @@ class _LoginState extends State<Login> {
               ),
               Row(
                 children: <Widget>[
-                  SizedBox(width: 100,),
+                  SizedBox(
+                    width: 100,
+                  ),
                   Icon(
                     Icons.lock,
                     color: Colors.green[900],
                     size: 350,
                   ),
-                  SizedBox(width: 80,),
-                  Container( // contains username and password fields
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Colors.blue,
-                    ),
-                    constraints: BoxConstraints.tightForFinite(
-                      width: 350,
-                      height: 50,
-                    ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Threat Model Evaluator',
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        // username field
+                        width: 350,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2),
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(30)),
+                              ),
+                              hintText: 'username'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        // password field
+                        width: 350,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2),
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(30)),
+                              ),
+                              hintText: 'password'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              // in progress: will be to view history
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ActionMenu()));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            color: Colors.blue,
+                          ),
+                          constraints: BoxConstraints.tightForFinite(
+                            width: 350,
+                            height: 50,
+                          ),
+                        ),
+                      ),
+                    ],
                   )
                 ],
               )
